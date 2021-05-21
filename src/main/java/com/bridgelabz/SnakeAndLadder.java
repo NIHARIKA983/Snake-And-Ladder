3ed826b79ed236d2ef5dd973ea120f21dd7f7177
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-05-21 19:46:50 +0530

    Added:To Access Random Die Roll To Get A Number Between 1 To 6

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,9 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     //variables
     int position=0;
     public static void main(String[] args){
         System.out.println("Welcome To Snake And Ladder Game");
+        SnakeAndLadder snakeLadder = new SnakeAndLadder();
+        snakeLadder.rollDice();
+    }
+    public int rollDice(){
+        Random random = new Random();
+        int dice = (random.nextInt(6)+1);
+        System.out.println("Die Roll Is :" + dice);
+        return dice;
     }
 }
